2f8d52839f8fde76412ff00e03af3808adf7340d
hainguyentran14@gmail.com <Admin@Admin>
2022-03-14 21:24:26 +0700

    update

--- src/main/java/module/Controller/IndexController.java
+++ src/main/java/module/Controller/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 	@RequestMapping("/")
 	public String index () {
-		return "redirect:/home";
+		return "user/index";
 	}
 	@RequestMapping({"/admin/**","/home/admins"})
 	public String admin () {
